61c09a1f849c9d53018e5cfb3e02cd7ae54ee3f0
Jon Hadfield <jon@lessknown.co.uk>
2016-04-17 23:31:39 +0100

    fix healthcheck (allow insecure https).

--- lib/taran/utils/web.py
+++ lib/taran/utils/web.py
@@ -3,19 +3,30 @@
 """Utilities relating to online services"""
 from __future__ import (print_function, unicode_literals)
 
+import time
+
 import requests
 
 
-def url_check(url=None, method='get', timeout=None, expected_codes=200):
+def url_check(url=None, method='get', timeout=None, expected_codes=200, interval=None, healthy_threshold=None):
     """Check if a request for a URL gives an expected response"""
     if expected_codes:
         if not isinstance(expected_codes, list):
             expected_codes = expected_codes,
         response = None
-        if method:
-            if method.lower() == 'get':
-                response = requests.get(url=url, timeout=timeout)
-            if method.lower() == 'head':
-                response = requests.head(url=url, timeout=timeout)
-        if response.status_code in expected_codes:
-            return True
+        wait_timeout = time.time() + timeout
+        healthy_count = 0
+        while wait_timeout > time.time():
+            try:
+                if method:
+                    if method.lower() == 'get':
+                        response = requests.get(url=url, timeout=10, verify=False)
+                    if method.lower() == 'head':
+                        response = requests.head(url=url, timeout=10, verify=False)
+                if response.status_code in expected_codes:
+                    healthy_count += 1
+                if healthy_count == healthy_threshold:
+                    return True
+            except:
+                healthy_count = 0
+                time.sleep(5)
