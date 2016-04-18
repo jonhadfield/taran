6f0288836bb5a6b48a3c9b28eb4da703d7f6423e
Jon Hadfield <jon@lessknown.co.uk>
2016-04-18 09:24:40 +0100

    add options to url_check and provide reasonable defaults.

--- lib/taran/utils/web.py
+++ lib/taran/utils/web.py
@@ -6,27 +6,27 @@ from __future__ import (print_function, unicode_literals)
 import time
 
 import requests
+from requests.exceptions import BaseHTTPError
 
 
-def url_check(url=None, method='get', timeout=None, expected_codes=200, interval=None, healthy_threshold=None):
+def url_check(url=None, method='get', timeout=None, response_timeout=2, expected_codes=200, interval=2,
+              healthy_threshold=1, ssl_verify=False):
     """Check if a request for a URL gives an expected response"""
-    if expected_codes:
-        if not isinstance(expected_codes, list):
-            expected_codes = expected_codes,
-        response = None
-        wait_timeout = time.time() + timeout
-        healthy_count = 0
-        while wait_timeout > time.time():
-            try:
-                if method:
-                    if method.lower() == 'get':
-                        response = requests.get(url=url, timeout=10, verify=False)
-                    if method.lower() == 'head':
-                        response = requests.head(url=url, timeout=10, verify=False)
-                if response.status_code in expected_codes:
-                    healthy_count += 1
-                if healthy_count == healthy_threshold:
-                    return True
-            except:
-                healthy_count = 0
-                time.sleep(5)
+    if not isinstance(expected_codes, list):
+        expected_codes = expected_codes,
+    response = None
+    wait_timeout = time.time() + timeout
+    healthy_count = 0
+    while wait_timeout > time.time():
+        try:
+            if method.lower() == 'get':
+                response = requests.get(url=url, timeout=response_timeout, verify=ssl_verify)
+            elif method.lower() == 'head':
+                response = requests.head(url=url, timeout=response_timeout, verify=ssl_verify)
+            if response.status_code in expected_codes:
+                healthy_count += 1
+            if healthy_count == healthy_threshold:
+                return True
+        except BaseHTTPError:
+            healthy_count = 0
+            time.sleep(interval)
